2f2edd8d7650c73597b3afe8b89d69b82d3c9620
juankuri <147097052+juankuri@users.noreply.github.com>
2025-12-06 22:05:34 -0600

    refactor(entities) removed unues imports from User

--- src/main/java/org/uv/SGLAC/entities/User.java
+++ src/main/java/org/uv/SGLAC/entities/User.java
@@ -1,6 +1,5 @@
 package org.uv.SGLAC.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -12,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
